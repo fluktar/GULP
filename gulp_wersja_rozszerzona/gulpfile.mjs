e158eaa6127ec99dbe6a0e2585e036ede3a875d1
Paweł <tyc.pawel@gmail.com>
2023-04-06 22:10:14 +0200

    update

---
+++ gulp_wersja_rozszerzona/gulpfile.mjs
@@ -0,0 +1,420 @@
+import gulp from 'gulp';
+import fs from 'fs';
+import cleanCSS from 'gulp-clean-css';
+import sassPackage from 'gulp-sass';
+import sassCompiler from 'sass';
+import uglify from 'gulp-uglify';
+import fileInclude from 'gulp-file-include';
+import browserSyncPackage from 'browser-sync';
+import rename from 'gulp-rename';
+import { promisify } from 'util';
+import { exec as childExec } from 'child_process';
+import ncu from 'npm-check-updates';
+import zip from 'gulp-zip';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const exec = promisify(childExec);
+const browserSync = browserSyncPackage.create();
+const sass = sassPackage(sassCompiler);
+
+function createFolders(done) {
+	const foldersToCreate = [
+		'dist',
+		'dist/css',
+		'dist/js',
+		'html',
+		'src',
+		'src/img',
+		'src/js',
+		'src/php',
+		'src/sass',
+		'instrukcja', // Dodanie folderu instrukcja
+	];
+
+	foldersToCreate.forEach(folder => {
+		if (!fs.existsSync(folder)) {
+			fs.mkdirSync(folder, { recursive: true });
+			console.log(`Folder "${folder}" został utworzony.`);
+		} else {
+			console.log(`Folder "${folder}" już istnieje.`);
+		}
+	});
+
+	done();
+}
+
+const copyImages = () => {
+	return gulp
+		.src('src/img/**/*')
+		.pipe(gulp.dest('dist/img'))
+		.pipe(browserSync.stream());
+};
+
+const checkFoldersExist = () => {
+	return new Promise((resolve, reject) => {
+		const folders = [
+			'dist',
+			'html',
+			'src',
+			'src/img',
+			'src/js',
+			'src/sass',
+			'src/php',
+		];
+
+		let allFoldersExist = true;
+		for (const folder of folders) {
+			if (!fs.existsSync(folder)) {
+				allFoldersExist = false;
+				break;
+			}
+		}
+
+		if (allFoldersExist) {
+			resolve();
+		} else {
+			reject();
+		}
+	});
+};
+
+const createFiles = done => {
+	const filesToCreate = [
+		{
+			path: 'html/index.kit',
+			content: `<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../dist/css/style.min.css">
+</head>
+<body>
+ 
+    <h1>Hello All !</h1>
+    <button id="menu-btn">Menu</button>
+    @@include('_nav.kit')
+    @@include('_footer.kit')
+    <script src="../dist/js/script.min.js"></script>
+</body>
+</html>`,
+		},
+		{
+			path: 'html/_footer.kit',
+			content: '<footer></footer>',
+		},
+		{
+			path: 'html/_nav.kit',
+			content: `<nav class="nav-menu" id="nav-menu">
+  <button class="close-btn" id="close-btn">X</button>
+  <ul>
+    <li><a href="#o-nas">O nas</a></li>
+    <li><a href="#nasz-zespol">Nasz zespół</a></li>
+    <li><a href="#nasze-osiagniecia">Nasze osiągnięcia</a></li>
+    <li><a href="#kontakt">Kontakt</a></li>
+  </ul>
+</nav>`,
+		},
+		{
+			path: 'src/sass/style.scss',
+			content: `* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+nav {
+  position: fixed;
+  top: 0;
+  left: -100%;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+  transition: left 0.3s ease-in-out;
+}
+
+nav.visible {
+  left: 0;
+}
+
+`,
+		},
+		{
+			path: 'src/js/script.js',
+			content: `'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const menuBtn = document.getElementById('menu-btn');
+  const closeBtn = document.getElementById('close-btn');
+  const nav = document.querySelector('nav');
+  const navLinks = document.querySelectorAll('nav a');
+
+  if (menuBtn && closeBtn && nav) {
+    menuBtn.addEventListener('click', () => {
+      nav.classList.add('visible');
+      nav.style.transform = 'translateY(0%)';
+    });
+
+    closeBtn.addEventListener('click', () => {
+      nav.classList.remove('visible');
+      nav.style.transform = 'translateY(-100%)';
+    });
+
+    // Dodanie event listenera do każdego elementu nawigacji
+    navLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        nav.classList.remove('visible');
+        nav.style.transform = 'translateY(-100%)';
+      });
+    });
+  }
+});
+
+`,
+		},
+		{
+			path: 'src/php/index.php',
+			content: '<?php     ',
+		},
+		{
+			path: 'instrukcja/instrukcja.md',
+			content: `# Instrukcja
+
+  ## Dodawanie plików zaimportowanych
+
+  Aby dodać plik zaimportowany, należy w \`index.kit\` dodać wpis \`@@include('_nav.kit')\`, a w utworzonym pliku dodać wpis np \`<nav></nav>\`.
+
+  ## Sprawdzanie aktualizacji pakietów
+
+  Aby sprawdzić aktualizacje pakietów i utworzyć plik z informacjami o aktualizacjach, wykonaj następujące kroki:
+
+  1. Otwórz terminal w katalogu głównym projektu.
+  2. Wpisz polecenie \`gulp checkPackageUpdates\`.
+  3. Zadanie sprawdzi dostępne aktualizacje pakietów i wyświetli informacjami o aktualizacjach.
+
+  Pamiętaj, aby regularnie sprawdzać aktualizacje, aby utrzymać swoje zależności na bieżąco.
+  
+ ## Tworzenie kopii zapasowej
+
+Aby utworzyć kopię zapasową folderów "dist", "html", "instrukcja", "src" oraz plików "gulpfile.mjs", ".gitignore" i "package.json", wykonaj następujące kroki:
+
+1. Upewnij się, że dodano funkcję "backupProject" oraz odpowiednie importy do pliku "gulpfile.mjs" oraz zdefiniowano nowe zadanie Gulp o nazwie "backup".
+
+2. Otwórz konsolę i przejdź do katalogu głównego projektu.
+
+3. Wpisz w konsoli polecenie: gulp backup
+
+4. Kopia zostanie wykonana "Z:_www"
+5. w Katalogu głównym sprawdz plik nr.txt w którym będzie unikalny numer backup utworzonego dla twojej kopii
+6. Utworzona kopia będzie miała nazwę z tym numerem
+
+## Pamiętaj o meta
+<meta
+      name="description"
+      content="Jesteśmy młodym "
+    />
+    <meta
+      name="keywords"
+      content="tworzenie stron www... "
+    />
+    <meta name="robots" content="index, follow">
+    <meta name="author" content="uroboros.online">
+ 
+  `,
+		},
+		{
+			path: '.gitignore', // Dodanie pliku .gitignore w katalogu głównym
+			content: `# Ignorowane pliki i foldery
+	node_modules/
+	src/
+	html/
+	gulpfile.mjs
+	package.json
+	package-lock.json
+	*.log`,
+		},
+	];
+
+	filesToCreate.forEach(file => {
+		if (!fs.existsSync(file.path)) {
+			fs.writeFileSync(file.path, file.content);
+			console.log(`Plik "${file.path}" został utworzony.`);
+		} else {
+			console.log(`Plik "${file.path}" już istnieje.`);
+		}
+	});
+	done();
+};
+
+const checkFoldersAndFiles = done => {
+	createFolders(done);
+	createFiles(done);
+	done();
+};
+
+const minifyJS = () => {
+	return gulp
+		.src('src/js/**/*.js')
+		.pipe(uglify())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(gulp.dest('dist/js'))
+		.pipe(browserSync.stream());
+};
+const checkPHP = () => {
+	return (
+		gulp
+			.src('src/php/**/*.php')
+			//   .pipe(uglify())
+			.pipe(gulp.dest('./'))
+			.pipe(browserSync.stream())
+	);
+};
+const compileKit = () => {
+	return gulp
+		.src(['html/**/*.kit', '!html/**/_*.kit'])
+		.pipe(
+			fileInclude({
+				prefix: '@@',
+				basepath: '@file',
+			})
+		)
+		.pipe(rename({ extname: '.html' }))
+		.pipe(gulp.dest('./'))
+		.on('end', () => {
+			console.log('Pliki .kit zostały skompilowane do .html');
+		});
+};
+
+function minifyCSS() {
+	return gulp
+		.src('src/sass/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(cleanCSS())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(gulp.dest('dist/css'))
+		.pipe(browserSync.stream());
+}
+
+const watch = () => {
+	browserSync.init({
+		server: {
+			baseDir: './',
+		},
+	});
+
+	gulp.watch('html/**/*.kit', compileKit);
+	gulp.watch('src/sass/**/*.scss', minifyCSS);
+	gulp.watch('src/js/**/*.js', minifyJS);
+	gulp.watch('src/php/**/*.php', checkPHP);
+	gulp.watch('src/img/**/*', copyImages);
+	gulp.watch('./*.html').on('change', browserSync.reload);
+};
+
+// ---------------------------------------------------------------
+
+async function checkPackageUpdates() {
+	try {
+		const upgraded = await ncu.run({
+			packageFile: 'package.json',
+			upgrade: false,
+		});
+
+		if (!Object.keys(upgraded).length) {
+			console.log('Wszystkie pakiety są aktualne.');
+		} else {
+			const updateInfo = Object.entries(upgraded)
+				.map(([key, value]) => `${key}: ${value}`)
+				.join('\n');
+			console.log(`Znaleziono aktualizacje pakietów:\n${updateInfo}`);
+			fs.writeFileSync('aktualizacja.txt', updateInfo);
+		}
+	} catch (error) {
+		console.error(
+			`Wystąpił błąd podczas sprawdzania aktualizacji pakietów: ${error}`
+		);
+	}
+}
+
+// ------------------------------------------------------------
+
+// --------------------------------------------------------------
+
+// PROJEKT BACKUP
+// --------------------------------------------------------------
+// --------------------------------------------------------------
+
+async function backupProject() {
+	const currentPath = fileURLToPath(import.meta.url);
+	const currentDir = path.dirname(currentPath);
+	const projectName = path.basename(currentDir);
+	const backupName = `${projectName}.zip`;
+	const outputDirectory = 'Z:/www';
+	const projectDirectory = path.join(outputDirectory, projectName);
+
+	// Sprawdź, czy istnieje folder projektu w katalogu docelowym
+	try {
+		await fs.promises.access(projectDirectory);
+	} catch {
+		// Utwórz folder, jeśli nie istnieje
+		await fs.promises.mkdir(projectDirectory);
+	}
+
+	// Usuń istniejący plik .zip w folderze projektu (jeśli istnieje)
+	const existingZipPath = path.join(projectDirectory, backupName);
+	try {
+		await fs.promises.access(existingZipPath);
+		await fs.promises.unlink(existingZipPath);
+		console.log(`Usunięto istniejący plik: ${existingZipPath}`);
+	} catch {
+		// Nic nie rób, jeśli plik .zip nie istnieje
+	}
+
+	return gulp
+		.src(
+			[
+				'dist/**/*',
+				'html/**/*',
+				'src/**/*',
+				'instrukcja/**/*',
+				'gulpfile.mjs',
+				'package.json',
+				'!node_modules/**/*',
+			],
+			{ base: '.', dot: true }
+		)
+		.pipe(zip(backupName))
+		.pipe(gulp.dest(projectDirectory))
+		.on('end', () => {
+			console.log(
+				`Kopia zapasowa została utworzona: ${projectDirectory}/${backupName}`
+			);
+		});
+}
+
+// --------------------------------------------------------------
+
+// --------------------------------------------------------------
+// --------------------------------------------------------------
+gulp.task('checkPackageUpdates', checkPackageUpdates);
+gulp.task('backup', backupProject);
+gulp.task('checkFoldersAndFiles', checkFoldersAndFiles);
+gulp.task('compileKit', gulp.series('checkFoldersAndFiles', compileKit));
+gulp.task('minifyCSS', minifyCSS);
+gulp.task('minifyJS', minifyJS);
+gulp.task('checkPHP', checkPHP);
+gulp.task('copyImages', copyImages);
+gulp.task(
+	'watch',
+	gulp.series(
+		'compileKit',
+		'minifyCSS',
+		'minifyJS',
+		'copyImages',
+		'checkPHP',
+		watch
+	)
+);
+
+gulp.task('default', gulp.series('watch'));
